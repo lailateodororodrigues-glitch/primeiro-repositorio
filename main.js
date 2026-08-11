03aa9ae83da94565e4083273fe3292a34c7ab862
lailateodororodrigues-glitch <laila.teodoro.rodrigues@escola.pr.gov.br>
2026-08-10 22:13:39 -0300

    10 08

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const botoes = document.querySelectorAll("button");
 
 botoes.forEach(function (botao) {
-    
-    let curtiu = false;
-    botao.addEventListener("click", botaoClicado);
-  function botaoClicado () 
+let curtiu = false;
+botao.addEventListener("click", botaoClicado);
+function botaoClicado () {
+console.log("fui clicado");
+let texto = botao.querySelector("span");
+if (curtiu === false) {
+texto.textContent++;
+curtiu = true;
+} else {
+texto.textContent--;
+curtiu = false;
+}
+}
+})
